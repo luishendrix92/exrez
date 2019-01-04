be399e3a66d710874eea53c7ad29c1173883491c
luishendrix92 <luishendrix92@gmail.com>
2019-01-04 15:30:20 -0800

    2.01 published fixed

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,5 @@ use Mix.Config
 # This is reserved for me, the ExRez developer!
 # ----------------------------------------------
 # config :exrez,
-#   dev_id: "2434",
-#   auth_key: "3BB8A39C64824E8ABCC5B4FEBDA968E6"
+#   dev_id: "XXXX",
+#   auth_key: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
